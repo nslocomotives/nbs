3621d152c00700a60d91a64293ab13621891ee91
Nigel Smith <nslocomotives@hotmail.com>
2018-04-08 10:58:24 +0100

    first commit of get statement data allowing import from first direct bank statements into a dictionary

---
+++ get_statement.py
@@ -0,0 +1,62 @@
+import glob
+import re
+import csv
+
+statementDir="D:\\banking\\statement\\"
+
+def extract_basename(path):
+  # Extracts basename of a given path. Should Work with any OS Path on any OS
+  basename = re.search(r'[^\\/]+(?=[\\/]?$)', path)
+  if basename:
+    return basename.group(0)
+
+def process_file(path, typeFlag):
+  # Collects the data from the CSV file according to the identified type and
+  # pushes the data into an array.
+  results = []
+  with open(path) as File:
+      data = csv.DictReader(File)
+      for row in data:
+          results.append(row)
+  return(results)
+
+def identify_file(p, f):
+  #find out if First Direct File (has "_" in it)
+  if f.find('_') >= 0:
+      str1 = f.find('_')
+      str2 = str1 + 1
+      date = f[:str1] #date file generated from filename
+      a = f[str2:]
+      str3 = a.find('.')
+      a = a[:str3] #set account number from file name
+      Flag = 'First Direct' #Type Flag for bank
+  else:
+      date = '00000000'
+      a = 'UNKNOWN'
+      Flag = 'UNKNOWN'
+  return(Flag, date, a)
+
+
+# ===Start of Program ===#
+
+# find all csv files and list them
+csvFilesStr = statementDir + "*.csv"
+csvFiles = glob.glob(csvFilesStr)
+
+for p in csvFiles:
+    print(p)
+    f = extract_basename(p)
+    (Flag, date, accountNumber) = identify_file(p, f)
+    #log out details of file
+    print('First Direct File: "' + f +'"')
+    print('File generated on: ' + date)
+    print('Account Number: ' + accountNumber)
+    if Flag != 'UNKNOWN':
+        #start work on the file
+        results = process_file(p, Flag)
+        print(results)
+    else:
+        #log out not able to work the file
+        print('Unknown File unable to parse')
+
+
