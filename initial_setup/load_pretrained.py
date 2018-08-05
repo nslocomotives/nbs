d2de66ac3768559ba00effdc4bfc7e4c5fbf4ecf
Nigel Smith <nslocomotives@hotmail.com>
2018-08-05 13:18:03 +0100

    adding process to create the embedings file from the source Glove data

---
+++ initial_setup/load_pretrained.py
@@ -0,0 +1,32 @@
+"""
+open pre-trained glove word embedding
+data from http://nlp.stanford.edu/projects/glove/
+"""
+# NOTE: to allow us to import modules from directory abouve we add it to the path with sysapend
+import sys
+sys.path.append("..")
+
+import os
+import numpy as np
+import pickle
+import get_config
+
+dirs = get_config.cfg['dirs']
+
+GLOVE_DIR = dirs['dataDir']
+model_name = dirs['dataDir'] + 'glove_embeddings'
+
+embeddings_index = {}
+f = open(os.path.join(GLOVE_DIR, 'glove.42B.300d.txt'))
+for line in f:
+    values = line.split()
+    word = values[0]
+    coefs = np.asarray(values[1:], dtype='float32')
+    embeddings_index[word] = coefs
+f.close()
+
+print('Found %s word vectors.' % len(embeddings_index))
+
+modelFileSave = open(model_name, 'wb')
+pickle.dump(embeddings_index, modelFileSave)
+modelFileSave.close()
